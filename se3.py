767f967ef533d2ea7a4f55e0214f4fd28f7757a9
Bruno Celaries <bruno.celaries@easymile.com>
2019-12-05 19:05:42 +0100

    Fix log map se3

--- se3.py
+++ se3.py
@@ -14,12 +14,15 @@ def V(angle, axis):
 def Vinv(angle, axis):
   Vinv = np.eye(3,3) - np.dot(0.5, so3.skew(axis))
   if angle * angle != 0:
-    Vinv = Vinv + (1/ (angle * angle)) * (1 - 
+    A = np.sin(angle) / angle
+    B = (1 - np.cos(angle)) / (angle * angle)
+    Vinv = Vinv + np.dot((1/ angle*angle), (1 - (A / (2*B)) * np.power(axis, 2)))
+  return Vinv
 
 def exp(tau):
   u = tau[0:3]
-  rvec = tau[3:7]
-  angle, axis = so3.rotv_to_angle_axis(rvec)
+  w = tau[3:7]
+  angle, axis = so3.rotv_to_angle_axis(w)
   R = so3.rodrigues(angle, axis)
   Vt = np.dot(V(angle,axis), u)
   T = np.zeros((4,4))
@@ -28,6 +31,10 @@ def exp(tau):
   return T
 
 def log(T):
-  R = so3.log_map_rot_matx(T[0:3, 0,3])
-  angle, axis = se3.log_map_rot_matx(R)
-  
+  angle, axis = so3.log_map_rot_matx(T[0:3, 0:3])
+  t = T[0:3, 3]
+  V_inv = Vinv(angle, axis)
+  tau = np.zeros(6)
+  tau[0:3] = np.dot(V_inv, t)
+  tau[3:7] = angle * axis
+  return tau
